f5ecceff20702570e36fa25711e8db588fd943d8
LogolsLLC <30915974+LogolsLLC@users.noreply.github.com>
2018-07-07 23:17:46 -0400

    modification to add status link

--- Exercises/Angular/ZombieReport/src/app/addStatus.component.ts
+++ Exercises/Angular/ZombieReport/src/app/addStatus.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { PersonStatus } from './entities/PersonStatus'
 import { PersonStatusService } from './services/PersonStatusService'
 
@@ -10,13 +11,14 @@ import { PersonStatusService } from './services/PersonStatusService'
 export class AddStatusComponent {
   public personStatus:PersonStatus;
 
-  public constructor(private personStatusService: PersonStatusService) {
+  public constructor(private personStatusService: PersonStatusService, private router: Router) {
     this.personStatus = new PersonStatus();
   }
 
   public insertClick():void {
     this.personStatusService.insert(this.personStatus).subscribe(() => {
       console.log("person inserted");
+      this.router.navigate(['report']);
     }, error => console.error(error));
   }
 }
